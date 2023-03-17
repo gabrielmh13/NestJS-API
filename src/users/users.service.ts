671ed0c7ee2ff5c2df0469ae94e7c15a56b396a2
gabrielmh13 <gabrielmh13@hotmail.com>
2023-03-17 20:57:15 -0300

    Finished CRUD

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,15 @@
-import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UserRole } from './user-roles.enum';
 import { UserRepository } from './users.repository';
 import { User } from './user.entity';
+import { UpdateUserDto } from './dtos/update-users.dto';
+import { FindUsersQueryDto } from './dtos/find-users-query.dto';
 
 @Injectable()
 export class UsersService {
@@ -14,5 +21,62 @@ export class UsersService {
     } else {
       return this.userRepository.createUser(createUserDto, UserRole.ADMIN);
     }
+  }
+
+  async findUserByid(userId: string): Promise<User> {
+    const user = await this.userRepository.findOne({
+      where: {
+        id: userId,
+      },
+      select: {
+        email: true,
+        name: true,
+        role: true,
+        id: true,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    return user;
+  }
+
+  async updateUser(updateUserDto: UpdateUserDto, id: string): Promise<User> {
+    const user = await this.findUserByid(id);
+    const { name, email, role, status } = updateUserDto;
+
+    user.name = name ? name : user.name;
+    user.email = email ? email : user.email;
+    user.role = role ? role : user.role;
+    user.status = status ? status : user.status;
+
+    try {
+      await user.save();
+      return user;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Erro ao salvar os dados no banco de dados',
+      );
+    }
+  }
+
+  async deleteUser(userId: string) {
+    const result = await this.userRepository.delete({ id: userId });
+
+    if (result.affected === 0) {
+      throw new NotFoundException(
+        'Não foi encontrado um usuário com o ID informado',
+      );
+    }
+  }
+
+  async findUsers(
+    queryDto: FindUsersQueryDto,
+  ): Promise<{ users: User[]; total: number }> {
+    const users = await this.userRepository.findUsers(queryDto);
+
+    return users;
   }
 }
